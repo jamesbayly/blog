20ec8579e07eee54fae46a7b055c62fe02622b61
James Bayly <james@bayly.xyz>
2025-03-27 17:32:53 +0800

    Inline styles

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -9,17 +9,29 @@ export default defineUserConfig({
   description: "A collection of musings and thoughts from the road.",
   theme,
   head: [
-    ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
     [
-      "link",
-      { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
-    ],
-    [
-      "link",
-      {
-        href: "https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap",
-        rel: "stylesheet",
-      },
+      "style",
+      {},
+      `\
+        /* latin */
+        @font-face {
+          font-family: 'Raleway';
+          font-style: italic;
+          font-weight: 100 900;
+          font-display: swap;
+          src: url(https://fonts.gstatic.com/s/raleway/v34/1Ptsg8zYS_SKggPNyCg4TYFqL_KWxQ.woff2) format('woff2');
+          unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        }
+          /* latin */
+        @font-face {
+          font-family: 'Raleway';
+          font-style: normal;
+          font-weight: 100 900;
+          font-display: swap;
+          src: url(https://fonts.gstatic.com/s/raleway/v34/1Ptug8zYS_SKggPNyC0IT4ttDfA.woff2) format('woff2');
+          unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        }
+      `,
     ],
   ],
   plugins: [
